0c05bf6980120a4c04aa3888ef2484f6fa3901c4
EliasTheBigMasterofIssues <eliascontato@protonmail.com>
2026-09-04 11:15:54 -0300

    desafios: desafio do algoritmo oito

---
+++ me-corrigido/AlgoritmoOito.java
@@ -0,0 +1,10 @@
+public class AlgoritmoOito {
+    public static void main(String[] args) {
+
+        int n1 = Integer.parseInt(IO.readln("\n" + "Digite o primeiro número: "));
+        int n2 = Integer.parseInt(IO.readln("\n" + "Digite o segundo número: "));
+
+        System.out.println("Este é o produto dos dois números: " +(n1*n2));
+
+    }
+}
