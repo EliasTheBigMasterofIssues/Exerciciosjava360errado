909d5f3f6f7e9d028aefe7c9ec64926d5be69b7f
EliasTheBigMasterofIssues <eliascontato@protonmail.com>
2026-09-04 11:30:16 -0300

    desafios: desafio algoritmo número dez

---
+++ me-corrigido/AlgoritmoDez.java
@@ -0,0 +1,10 @@
+public class AlgoritmoDez {
+    public static void main(String[] args) {
+
+        float n1 = Float.parseFloat(IO.readln("\n" + "Digite o primeiro número: "));
+        float n2 = Float.parseFloat(IO.readln("\n" + "Digite o segundo número: "));
+
+        System.out.println("\n" + " Esta é a média aritmética dos seus dois números: " + ((n1+n2)/4) + "\n");
+
+    }
+}
