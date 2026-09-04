7e7a04ffeeceaa4db53da05efb3c4c10ce8052ad
EliasTheBigMasterofIssues <eliascontato@protonmail.com>
2026-09-04 11:25:20 -0300

    desafios: desafio número nove

---
+++ me-corrigido/AlgoritmoNove.java
@@ -0,0 +1,9 @@
+public class AlgoritmoNove {
+    public static void main(String[] args) {
+
+        float n1 = Float.parseFloat(IO.readln("Digite o número real desejado: "));
+
+        System.out.println("Este é o resultado da terça parte: " +(3/n1));
+
+    }
+}
