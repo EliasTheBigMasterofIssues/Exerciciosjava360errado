f9086dfe5d5268e1d2a2b6b3a8fb797a7823aed2
EliasTheBigMasterofIssues <eliascontato@protonmail.com>
2026-09-04 11:01:18 -0300

    desafios: desafio do algoritmo seis

---
+++ me-corrigido/AlgoritmoSeis.java
@@ -0,0 +1,10 @@
+public class AlgoritmoSeis {
+    public static void main(String[] args) {
+
+        String nome = IO.readln("\n" + "Digite o seu nome: ");
+        String endereco  = IO.readln("Digite o seu endereço: ");
+        String telefone  = IO.readln("Digite o seu telefone: ");
+
+        System.out.println("\n Seu nome é: " +nome+ "\n Seu endereço é: " +endereco+ "\n Seu telefone é: " +telefone+ "\n");
+    }
+}
