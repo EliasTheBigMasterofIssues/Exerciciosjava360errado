c9f62f3ed051c4e7946fd214e86403d41f4bc05d
EliasTheBigMasterofIssues <eliascontato@protonmail.com>
2026-09-04 11:09:32 -0300

    desafios: desafio algoritmo sete

---
+++ me-corrigido/AlgoritmoSete.java
@@ -0,0 +1,12 @@
+
+
+public class AlgoritmoSete {
+    public static void main(String[] args) {
+
+        int n1 = Integer.parseInt(IO.readln("\n" + "Digite o primeiro número: "));
+        int n2 = Integer.parseInt(IO.readln("\n" + "Digite o segundo número: "));
+
+        System.out.println("\n" + "Soma: " + "\n" + " O resultado da soma é: " +(n1+n2) + "\n");
+
+    }
+}
