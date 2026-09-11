fda9775f99d808f5921a08f048c2a0438ece9fcc
EliasTheBigMasterofIssues <eliascontato@protonmail.com>
2026-09-11 11:06:31 -0300

    me: Exercicio número Quinze

---
+++ me-corrigido/AlgoritmoQuinze.java
@@ -0,0 +1,10 @@
+public class AlgoritmoQuinze {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 200; i++) {
+            String resultado = (i%2==0) ? (""+i) : "";
+            System.out.println(resultado);
+        }
+
+    }
+}
