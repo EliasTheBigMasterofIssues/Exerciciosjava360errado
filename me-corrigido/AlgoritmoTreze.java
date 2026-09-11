1087573f1f127861ad4511686d53be8df0571922
EliasTheBigMasterofIssues <eliascontato@protonmail.com>
2026-09-11 10:44:13 -0300

    me: exercicio número treze

---
+++ me-corrigido/AlgoritmoTreze.java
@@ -0,0 +1,10 @@
+public class AlgoritmoTreze {
+    public static void main(String[] args) {
+
+        int n1 = Integer.parseInt(IO.readln("Digite o primeiro número: "));
+        int n2 = Integer.parseInt(IO.readln("Digite o segundo número: "));
+        String resultado = (n2>=n2) ? "O Segundo número é maior ou igual ao primeiro" : "O Segundo número é menor do que o primeiro";
+        System.out.println(resultado);
+
+    }
+}
