a710664d47cae3a9a083ff526b543dbaab51ae54
EliasTheBigMasterofIssues <eliascontato@protonmail.com>
2026-09-11 11:30:08 -0300

    me: Exercicio número dezenove

---
+++ me-corrigido/AlgoritmoDezenove.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+public class AlgoritmoDezenove {
+    public static void main(String[] args) {
+
+        List<String> usuario = List.of("Ana", "Bruno", "Carla", "Diego");
+
+        usuario.forEach(s -> System.out.println("Mensagem recebida de: " +s));
+
+    }
+}
