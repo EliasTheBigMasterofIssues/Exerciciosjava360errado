7d58b490f402fffcfc0d704e85f61f17fef6b835
EliasTheBigMasterofIssues <eliascontato@protonmail.com>
2026-09-11 09:57:28 -0300

    me: exercicio algoritmo número onze

---
+++ me-corrigido/AlgoritmoOnze.java
@@ -0,0 +1,12 @@
+public class AlgoritmoOnze {
+    public static void main() {
+
+        // Entrada
+        String nome = IO.readln("Digite o seu nome: ");
+        String resultado = (nome.equalsIgnoreCase("Elias")) ? "Meu nome está correto" : "Meu nome está errado";
+        // Verificação
+        System.out.println(resultado);
+
+
+    }
+}
