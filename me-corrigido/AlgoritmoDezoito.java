48891b69a7307503000bd0e319b12e8a6a662940
EliasTheBigMasterofIssues <eliascontato@protonmail.com>
2026-09-11 11:22:38 -0300

    me: Exercicio Algoritmo número dezoito

---
+++ me-corrigido/AlgoritmoDezoito.java
@@ -0,0 +1,14 @@
+public class AlgoritimoDezoito {
+
+    public static void main(String[] args) {
+
+        int vetor[] = new int[201];
+
+        for (int i = 0; i <= 200; i++) {
+            vetor[i] = i;
+        }
+        for (int i = 0; i <= 200; i++) {
+            System.out.println("\n" +vetor[i]);
+        }
+    }
+}
